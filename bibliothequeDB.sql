8c81df0f8d0cbaa6d898bdb342d5f9b7d84430bd
Deffo Ivan <deffoivan2004@gmail.com>
2024-07-23 19:08:15 +0100

    modif1ererequete

--- bibliothequeDB.sql
+++ bibliothequeDB.sql
@@ -223,7 +223,6 @@ delimiter ;
 
 # requête pour trouver les étudiants ayant emprunter un certain livre
 SELECT e.id_etudiant, e.nom, e.prénom, e.date_naiss, e.email, e.tel, e.date_inscription FROM étudiants e JOIN emprunts em ON e.id_etudiant = em.id_etudiant_étudiants JOIN livres l ON em.id_livre_livres = l.id_livre WHERE l.id_livre = 3;
-
 # Requête pour lister les emprunts en cours d_un étudiant
 SELECT em.date_empt, em.date_retour, em.retour, l.titre, l.auteur FROM emprunts em JOIN livres l ON em.id_livre_livres = l.id_livre WHERE em.id_etudiant_étudiants = 3 AND em.retour = FALSE;
 
