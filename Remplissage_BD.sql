c0526617aec3d86ef0a92643b2dc3aae1319fe22
stacvirus <parfaitandre@gmail.com>
2024-07-23 15:23:41 +0100

    data insertion done

--- Remplissage_BD.sql
+++ Remplissage_BD.sql
@@ -4,12 +4,8 @@ INSERT INTO genres(nom_genre)
 		("Adventure"),
 		("romances"),
 		("mystery"),
-		(Whitchcraft");
+		("Whitchcraft");
 
-
-
-
-	
 INSERT INTO étudiants(nom, prénom, date_naiss, email,tel, date_inscription )
 	VALUES("Kamga","kotopi","1997-10-23", "kotopi@gmail.com", "698741234","2022-03-05"),
 		("Siewe","Mignon","1999-05-23", "Mignon@gmail.com", "691547834","2022-10-15"),
@@ -21,8 +17,6 @@ INSERT INTO étudiants(nom, prénom, date_naiss, email,tel, date_inscription )
 		("Tenor","Ellie","1997-07-14", "ellie@gmail.com", "691789634","2022-01-21"),
 		("Kella","Joyce","1997-08-07", "kella@gmail.com", "694754534","2022-05-08"),
 		("Tegang","karlyne","1997-10-12", "karlyne@gmail.com", "693713134","2022-07-27");
-
-
 
 
 INSERT INTO livres(auteur, titre, année_public, nbr_exemplaire,id_genre_genres)
@@ -43,25 +37,25 @@ INSERT INTO livres(auteur, titre, année_public, nbr_exemplaire,id_genre_genres)
 		("C.G. Jung","Memories Dreams Reflections",1961,35, 1),
 		("Philip Roth","Goodbye, Columbus",1959,9, 3),
 		("Malcolm X and Alex Haley","Autobiography",1964,4, 3),
-		("Jule Eisenbud, M.D.",The World of Ted Serios,1968,3, 1),
+		("Jule Eisenbud, M.D.","The World of Ted Serios",1968,3, 1),
 		("Penelope Gilliatt","A State of Change",1967,22, 3);
 		
 INSERT INTO emprunts(date_empt, date_retour, retour, id_livre_livres, id_etudiant_étudiants)
-	VALUES(2024-06-24 ,2024-06-28, TRUE, 2 ,3),
-		(2024-06-21, 2024-06-23, TRUE, 4 ,8),
-		(2024-03-25, 2024-03-28, TRUE, 5 ,6),
-		(2024-06-23, 2024-06-24, TRUE, 3 ,7),
-		(2024-06-20, 2024-06-25, TRUE, 9 ,6),
-		(2024-06-12, 2024-06-14, TRUE, 1 ,7),
-		(2024-06-04, 2024-06-06,TRUE, 2,9),
-		(2024-06-01, 2024-06-12, TRUE, 1 ,3),
-		(2024-06-02, 2024-06-10, TRUE, 2 ,1),
-		(2024-06-05, 2024-06-10, TRUE, 2 ,2),
-		(2024-06-12, 2024-06-15, TRUE, 3 ,5),
-		(2024-06-04, 2024-06-05, TRUE, 7,7),
-		(2024-06-11, 2024-06-18, TRUE, 3,9),
-		(2024-06-22, 2024-06-28, TRUE, 2,10),
-		(2024-06-21, 2024-06-28, TRUE, 5 ,9);
+	VALUES("2024-06-24" ,"2024-07-28", false, 2 ,3),
+		("2024-06-21", "2024-06-23", TRUE, 4 ,8),
+		("2024-03-25", "2024-03-28", TRUE, 5 ,6),
+		("2024-06-23", "2024-06-24", TRUE, 3 ,7),
+		("2024-06-20", "2024-06-25", TRUE, 9 ,6),
+		("2024-06-12", "2024-08-14", false, 1 ,7),
+		("2024-06-04", "2024-010-06",false, 2,9),
+		("2024-06-01", "2024-11-12", false, 1 ,3),
+		("2024-06-02", "2024-8-10", false, 2 ,1),
+		("2024-06-05", "2024-09-10", false, 2 ,2),
+		("2024-06-12", "2024-06-15", TRUE, 3 ,5),
+		("2024-06-04", "2024-06-05", TRUE, 7,7),
+		("2024-06-11", "2024-08-18", false, 3,9),
+		("2024-06-22", "2024-06-28", TRUE, 2,10),
+		("2024-06-21", "2024-07-28", false, 5 ,9);
 		
 	
 
